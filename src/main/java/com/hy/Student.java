f544385e3bf01db28211be961f856237020ba780
yaomin <493614571@qq.com>
2019-03-14 17:41:35 +0800

    提交

---
+++ src/main/java/com/hy/Student.java
@@ -0,0 +1,27 @@
+package com.hy;
+
+/**
+ * @Author: Candy
+ * @Date: 2019/3/14 17:39
+ * @Description:
+ */
+public class Student {
+    private Integer stuid;
+    private String stuname;
+
+    public Integer getStuid() {
+        return stuid;
+    }
+
+    public void setStuid(Integer stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getStuname() {
+        return stuname;
+    }
+
+    public void setStuname(String stuname) {
+        this.stuname = stuname;
+    }
+}
